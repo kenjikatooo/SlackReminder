86dbf41bafed0dca26ecb31795ae52a8b5095d48
kenjikatooo <k.524.10nis@gmail.com>
2019-01-25 16:45:14 +0900

    👍 arrange message

--- src/index.js
+++ src/index.js
@@ -6,9 +6,17 @@ exports.handler = async (event, context) => {
         const url = `https://slack.com/api/chat.postMessage`;
         const params = {
             "channel": process.env.CHANNEL_ID,
-            "text": "Slack Reminder Message", // Set any messages as you like
-            "icon_emoji": ":slack:", // Set any icon_emoji as you like
-            "username": "Slack Reminder Name" // Set any names as you like
+            "text": "
+                本日はプロダクトミーティングです :docker_christmas:  1週間お疲れ様でした:tada:  
+
+                定例アジェンダの公開とメンバーアジェンダの公募をします！
+                mtgでやりたいことや言いたいことがある人はこのスレッドに所要時間と概要を書いてください:woman-gesturing-ok:
+
+                1. タスク整理＆BUMP👊 20minutes
+                2. 来週やること決定📝 20minutes
+                3. 持ち込みアジェンダ🏃‍♂️ 15minutes", // Set any messages as you like
+            "icon_emoji": ":yatteki:", // Set any icon_emoji as you like
+            "username": "Mutta Namba" // Set any names as you like
         }
 
         const config = {
